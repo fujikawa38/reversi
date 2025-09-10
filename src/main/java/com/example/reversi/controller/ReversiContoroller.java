e256bf1b184d27505a7d9de26bfd170ed4e0814b
fujikawa38 <tacchi.juc.08lastwin@gmail.com>
2025-09-11 00:15:01 +0900

    FXMLファイルとコントローラークラスを紐づけ

--- src/main/java/com/example/reversi/controller/ReversiContoroller.java
+++ src/main/java/com/example/reversi/controller/ReversiContoroller.java
@@ -2,7 +2,22 @@ package com.example.reversi.controller;
 
 import org.springframework.stereotype.Controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+
 @Controller
 public class ReversiContoroller {
+
+	@FXML
+	private GridPane boardGrid;
+
+	@FXML
+	private Label statusLabel;
+
+	@FXML
+	public void initialize() {
+
+	}
 
 }
