21d3ab391e50bb5b289516178e93245e8d899ba2
fujikawa38 <tacchi.juc.08lastwin@gmail.com>
2025-09-11 00:03:37 +0900

    springbootとjavafx利用のためのgradleの設定 初期コントローラーとfxmlファイルの作成

--- src/main/java/com/example/reversi/ReversiApplication.java
+++ src/main/java/com/example/reversi/ReversiApplication.java
@@ -2,12 +2,43 @@ package com.example.reversi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 @SpringBootApplication
-public class ReversiApplication {
+public class ReversiApplication extends Application {
+
+	private ConfigurableApplicationContext springContext;
+	private Parent root;
+
+	@Override
+	public void init() throws Exception {
+		springContext = SpringApplication.run(ReversiApplication.class);
+		FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/reversi.fxml"));
+		fxmlLoader.setControllerFactory(springContext::getBean);
+		root = fxmlLoader.load();
+	}
+
+	@Override
+	public void start(Stage stage) throws Exception {
+		stage.setTitle("リバーシ");
+		Scene scene = new Scene(root, 600, 600);
+		stage.setScene(scene);
+		stage.show();
+	}
+
+	@Override
+	public void stop() throws Exception {
+		springContext.close();
+	}
 
 	public static void main(String[] args) {
-		SpringApplication.run(ReversiApplication.class, args);
+		launch(args);
 	}
 
 }
